ef8356084ccceabc972494868af7728ee449011d
Enderman1235084 <enderman1235084@gmail.com>
2025-07-04 06:59:56 +0530

    switching back

--- script.js
+++ script.js
@@ -28,37 +28,3 @@ function toggleTheme() {
             break;
     }
 }
-
-async function updateDynamicPage() {
-    const response = await fetch("/pages/pagemaps.json");
-    if (!response.ok) {
-        console.error("Error with fetch, status text: " + response.statusText);
-        return;
-    }
-    const pagemaps = await response.json();
-    if (window.location.pathname.slice(1) in pagemaps) {
-        jsjs.load(pagemaps[window.location.pathname.slice(1)]);
-    }
-    else {
-        jsjs.load({
-            html: "/pages/404/404.html",
-            css: "/pages/404/404.css",
-            js: "/pages/404/404.js"
-        });
-    }
-}
-
-window.onpopstate = updateDynamicPage;
-
-window.addEventListener("DOMContentLoaded", event => {
-    const navLinks = document.querySelectorAll("a");
-    jsjs.dynamicElement = document.querySelector("main");
-    updateDynamicPage();
-    for (let link of navLinks) {
-        link.addEventListener("click", event => {
-            event.preventDefault();
-            updateDynamicPage();
-            history.pushState(null, "",  link.getAttribute("href"));
-        });
-    }
-});
